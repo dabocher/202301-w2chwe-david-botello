31c1e2bd0e82b078dcf14cfc0832e8881c917597
David Botello <dabocher@gmail.com>
2023-01-21 14:14:29 +0100

    Fix index.js add next-generation algoritm

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,19 @@
-const a = 0;
+const board = [
+  [1, 0, 1],
+  [0, 1, 1],
+  [1, 0, 0],
+];
+let cellsAlive = 0;
 
-document.write(a);
+for (const row of board) {
+  cellsAlive += row.reduce((total, cell) => total + cell, 0);
+}
+
+if (board[1][1] === 1) {
+  board[1][1] = 0;
+  if (cellsAlive >= 3 && cellsAlive <= 4) {
+    board[1][1] = 1;
+  }
+} else if (board[1][1] === 0 && cellsAlive === 3) {
+  board[1][1] = 1;
+}
